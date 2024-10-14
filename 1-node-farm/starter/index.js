1d3f217d2cf045499d68878bc29dc01a4bfbcb3f
SwellTugboat573 <zacharysummers573@gmail.com>
2024-10-14 22:06:46 +1100

    section 2 complete

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,12 +1,13 @@
-const fs = require("fs"); // the file system module
-const http = require("http");
-const url = require("url");
-
+const fs = require('fs'); // the file system module
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTempalte');
 ////////////////////////////////////////////////////////////////////////////////
 // FILE
 
 // // blocking, synchornous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
 
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
@@ -32,66 +33,59 @@ const url = require("url");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Server
-// iterates over the products json which has been converted to html. then is made to a let variable so it can be manipulated without affecting the main data. then the output.replace allows us to replace all ocurrances of the placeholders added thanks to the "/{}/g" syntac
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataobj = JSON.parse(data);
 
+const slugs = dataobj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // overview page
-  if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataobj
       .map((el) => replaceTemplate(tempCard, el))
-      .join("");
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
 
     //Product Page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT!)");
+  } else if (pathname === '/product') {
+    const product = dataobj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    res.end(output);
     // API
-  } else if (pathName === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
     // Not FOUND
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello world",
+      'Content-type': 'text/html',
+      'my-own-header': 'hello world',
     });
-    res.end("<h1>page not found ! </h1> ");
+    res.end('<h1>page not found ! </h1> ');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Server is running on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server is running on port 8000');
 });
